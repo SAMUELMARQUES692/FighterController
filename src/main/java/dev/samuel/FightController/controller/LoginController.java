261927334ac15baff78f906485f29107a09009fc
samuel <samue@Samuel>
2026-06-06 15:31:54 -0300

    feat: Implementa o metodo login na LoginController

--- src/main/java/dev/samuel/FightController/controller/LoginController.java
+++ src/main/java/dev/samuel/FightController/controller/LoginController.java
@@ -1,6 +1,12 @@
 package dev.samuel.FightController.controller;
 
+import dev.samuel.FightController.request.LoginRequest;
+import dev.samuel.FightController.response.LoginResponse;
+import dev.samuel.FightController.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class LoginController {
 
+    private final LoginService loginService;
 
-
+    @PostMapping
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(loginService.login(request));
+    }
 }
